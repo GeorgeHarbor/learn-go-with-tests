3c3aff9b0b94d4f83c01ba6822ca7d91bf490c65
GeorgeHarbor <ldjordjevic@protonmail.com>
2024-02-24 22:16:58 +0100

    feature: Add language support and corresponding tests

--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -4,18 +4,30 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Luka")
+		got := Hello("Luka", "")
 		want := "Hello, Luka"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' when empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 
 		assertCorrectMessage(t, got, want)
 	})
+
+  t.Run("in Serbian", func(t *testing.T){
+    got := Hello("Luka", "Serbian")
+    want := "Zdravo, Luka"
+    assertCorrectMessage(t, got, want)
+  })
+  
+  t.Run("in Spanish", func(t *testing.T){
+    got := Hello("Luka", "Spanish")
+    want := "Hola, Luka"
+    assertCorrectMessage(t, got, want)
+  })
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
