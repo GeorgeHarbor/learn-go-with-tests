f042905462b527e73664393cce0456fef912037b
GeorgeHarbor <ldjordjevic@protonmail.com>
2024-02-24 21:29:28 +0100

    feat: Handle empty string in Hello function and add corresponding tests

--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -4,11 +4,23 @@ import "testing"
 
 func TestHello(t *testing.T)  {
   
+  t.Run("saying hello to people", func(t *testing.T){
+    
   got := Hello("Luka")
   want := "Hello, Luka"
 
   if got != want {
     t.Errorf("got %q want %q", got, want)
   }
+  })
+  
+  t.Run("say 'Hello, world' when empty string is supplied", func(t *testing.T){
+    got := Hello("")
+    want := "Hello, world"
+
+    if got != want {
+      t.Errorf("got %q want %q", got, want)
+    }
+  })
 }
 
